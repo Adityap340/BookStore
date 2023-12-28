84ca10846d6793609dff7a1ce7e853fde22d9390
Adityap340 <99486698+Adityap340@users.noreply.github.com>
2023-12-28 15:25:16 +0530

    Update PrivateRoute.jsx

--- mern-client/src/PrivateRoute/PrivateRoute.jsx
+++ mern-client/src/PrivateRoute/PrivateRoute.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
 import { AuthContext } from '../context/AuthProvider';
 
-const PrivateRoute = ({ ...rest }) => {
+const PrivateRoute = () => {
   const { user, loading } = useContext(AuthContext);
   const location = useLocation();
 
